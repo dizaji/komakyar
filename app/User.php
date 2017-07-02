bc3b230a362a8be71e77a16c50b46d9cc230c204
Mohammad <m.nourinik@irantalent.com>
2017-07-02 22:24:07 +0430

    create Students model and relations

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function student()
+    {
+        return $this->hasOne('App\Models\User\Student', 'user_id', 'id');
+    }
 }
