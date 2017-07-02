0cccb5114e81dcff2f3dc4c273e2c2e5b1bf17ff
Mohammad <m.nourinik@irantalent.com>
2017-07-02 22:20:31 +0430

    create user_students table

---
+++ database/migrations/2017_07_02_174508_create_students_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->unsignedInteger('user_id');
+            $table->string('student_code')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('mobile')->nullable();
+            $table->text('address')->nullable();
+        });
+
+        Schema::table('user_students', function (Blueprint $table) {
+            $table->foreign('user_id', 'fk_user_student_1')->references('id')->on('users')->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_students', function (Blueprint $table) {
+            $table->dropForeign('fk_user_student_1');
+        });
+
+        Schema::dropIfExists('students');
+    }
+}
