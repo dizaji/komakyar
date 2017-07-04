6a5efd21b34643b2e366046df011e9f2b1ef046e
Mohammad <m.nourinik@irantalent.com>
2017-07-05 00:06:53 +0430

    create course model

---
+++ app/Models/Education/Course.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models\Education;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    protected $table = 'education_courses';
+
+    public function courseSource()
+    {
+        return $this->belongsTo('App\Models\Lookup\CourseSource', 'course_source_id', 'id');
+    }
+
+    public function levelField()
+    {
+        $this->belongsTo('App\Models\LevelField', 'level_field_id', 'id');
+    }
+
+    public function academicYear()
+    {
+        $this->belongsTo('App\Models\AcademicYear', 'academic_year_id', 'id');
+    }
+}
