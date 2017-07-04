e3cd7cc36b6cbbdc8c0458b98756198b38ad7494
Mohammad <m.nourinik@irantalent.com>
2017-07-04 23:57:14 +0430

    create education courses table

---
+++ database/migrations/2017_07_04_191903_create_courses_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('education_courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->unsignedInteger('lookup_course_source_id');
+            $table->unsignedInteger('level_field_id');
+            $table->unsignedInteger('academic_year_id');
+
+            $table->unsignedTinyInteger('multiplier')->default(0);
+        });
+
+        Schema::table('education_courses', function (Blueprint $table) {
+            $table->foreign('lookup_course_source_id', 'fk_education_courses_1')->references('id')->on('lookup_course_sources')->onUpdate('CASCADE');
+            $table->foreign('level_field_id', 'fk_education_courses_2')->references('id')->on('level_fields')->onUpdate('CASCADE');
+            $table->foreign('academic_year_id', 'fk_education_courses_3')->references('id')->on('academic_years')->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('education_courses', function (Blueprint $table) {
+            $table->dropForeign('fk_education_courses_1');
+            $table->dropForeign('fk_education_courses_2');
+            $table->dropForeign('fk_education_courses_3');
+        });
+
+        Schema::dropIfExists('education_courses');
+    }
+}
