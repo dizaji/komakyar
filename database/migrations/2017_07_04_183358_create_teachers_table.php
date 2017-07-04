ce4e7ee61240567527c490d2b6fb5db6df4ca4df
Mohammad <m.nourinik@irantalent.com>
2017-07-04 23:06:17 +0430

    create teachers table

---
+++ database/migrations/2017_07_04_183358_create_teachers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->enum('degree', ['below diploma', 'diploma', 'BsC', 'MsC', 'PhD', 'Other'])->nullable();
+            $table->string('field_of_study')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('is_visible')->default(true);
+        });
+
+        Schema::table('user_teachers', function (Blueprint $table) {
+            $table->foreign('user_id', 'fk_user_teachers_1')->references('id')->on('users')->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_teachers', function (Blueprint $table) {
+            $table->dropForeign('fk_user_teachers_1');
+        });
+
+        Schema::dropIfExists('user_teachers');
+    }
+}
