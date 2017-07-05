b47b8b962d5351f510333aa33185d720c6ec8e1f
Mohammad <m.nourinik@irantalent.com>
2017-07-06 00:51:37 +0430

    create GradeType model

---
+++ app/Models/User/Teacher/GradeType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\User\Teacher;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GradeType extends Model
+{
+    protected $table = 'user_teacher_grade_types';
+
+    public function teacher()
+    {
+        return $this->belongsTo('App\Models\User\Teacher\Teacher', 'teacher_id', 'id');
+    }
+
+    public function courseSource()
+    {
+        return $this->belongsTo('App\Models\Lookup\CourseSource', 'course_source_id', 'id');
+    }
+}
