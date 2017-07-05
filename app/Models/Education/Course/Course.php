dd7e672b714128be771d80bc924558d0ec6313a8
Mohammad <m.nourinik@irantalent.com>
2017-07-06 00:53:59 +0430

    fix wording in courses model

--- app/Models/Education/Course/Course.php
+++ app/Models/Education/Course/Course.php
@@ -23,7 +23,7 @@ class Course extends Model
         $this->belongsTo('App\Models\AcademicYear', 'academic_year_id', 'id');
     }
 
-    public function Presentation()
+    public function Presentations()
     {
         return $this->hasMany('App\Models\Education\Course\Presentation', 'course_id', 'id');
     }
