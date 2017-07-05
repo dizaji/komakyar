fa5e4906970429e15e1f687b4f1297ed0bb81c0d
Mohammad <m.nourinik@irantalent.com>
2017-07-06 00:51:18 +0430

    create user_teacher_grade_types table

---
+++ database/migrations/2017_07_05_201123_create_teacher_grade_types_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeacherGradeTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_teacher_grade_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->unsignedInteger('teacher_id');
+            $table->unsignedInteger('course_source_id');
+
+            $table->string('title');
+            $table->integer('maximum_mark')->default(20);
+            $table->integer('minimum_mark')->default(0);
+            $table->float('step')->default(1);
+            $table->boolean('is_visible')->default(true);
+        });
+
+        Schema::table('user_teacher_grade_types', function (Blueprint $table) {
+            $table->foreign('teacher_id', 'fk_user_teacher_grade_types_1')->references('id')->on('user_teachers')->onUpdate('CASCADE');
+            $table->foreign('course_source_id', 'fk_user_teacher_grade_types_2')->references('id')->on('lookup_course_sources')->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_teacher_grade_types', function (Blueprint $table) {
+            $table->dropForeign('fk_user_teacher_grade_types_1');
+            $table->dropForeign('fk_user_teacher_grade_types_2');
+        });
+
+        Schema::dropIfExists('user_teacher_grade_types');
+    }
+}
