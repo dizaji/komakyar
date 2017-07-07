99c1fff6d3b2bfe05eae2e5c4601eaae3c6e9ae8
Mohammad <m.nourinik@irantalent.com>
2017-07-07 13:08:46 +0430

    create DailyProgram Model

---
+++ app/Models/Education/Program/DailyProgram.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models\Education\Program;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DailyProgram extends Model
+{
+    protected $table = "education_daily_programs";
+
+    public function presentation()
+    {
+        return $this->belongsTo('App\Models\Education\Course\Presentation', 'presentation_id', 'id');
+    }
+
+    public function staff()
+    {
+        return $this->belongsTo('App\Models\User\Staff', 'staff_id', 'id');
+    }
+
+    public function group()
+    {
+        return $this->belongsTo('App\Models\Education\Group\Group', 'group_id', 'id');
+    }
+}
