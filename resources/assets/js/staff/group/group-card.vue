46a9eb8286d52ca1400501b1ee5640aede8dcc63
Mohammad <m.nourinik@irantalent.com>
2017-07-21 15:19:18 +0430

    modify public folder

---
+++ resources/assets/js/staff/group/group-card.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <header-component/>
+        <div>this is template body</div>
+        <other-component/>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+    }
+</script>
---
+++ resources/assets/js/staff/group/group-list.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <group-card></group-card>
+    </div>
+</template>
+<script>
+    import GroupCard from './group-card.vue'
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+            'group-card': GroupCard,
+        }
+    }
+
+</script>
