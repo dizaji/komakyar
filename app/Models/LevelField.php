c32059d58578a3150a56ac8758f768870a9b7efd
Mohammad <m.nourinik@irantalent.com>
2017-07-24 00:37:16 +0430

    fix wording of eloquent relations in levelField model

--- app/Models/LevelField.php
+++ app/Models/LevelField.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class LevelField extends Model
 {
-    public function levels()
+    public function level()
     {
         return $this->belongsTo('App\Models\Lookup\Level', 'level_id', 'id');
     }
 
-    public function fields()
+    public function field()
     {
         return $this->belongsTo('App\Models\Lookup\Field', 'field_id', 'id');
     }
