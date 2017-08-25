0e9f289cebb5d3a3e721b062e1552bbcd5dae85d
Mohammad Nourinik <mohammad@inpin.co>
2017-08-25 09:48:48 +0430

    add not_present alpha_spaces and alpha_number_spaces validation rules

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('not_present', function($attribute, $value, $parameters)
+        {
+            return false;
+        });
+
+        /*
+         * Validates only alphabetic and spaces
+         */
+        Validator::extend('alpha_spaces', function ($attribute, $value) {
+            return preg_match('/^[\pL\s]+$/u', $value);
+        });
+
+        /*
+         * Validates only alphabetic and spaces
+         */
+        Validator::extend('alpha_number_spaces', function ($attribute, $value) {
+            return preg_match('/^[\pL\s\d]+$/u', $value);
+        });
     }
 
     /**
