f09c9ad08427221e55c1a837d88fd1a6c61f5599
Mohammad Nourinik <mohammad@inpin.co>
2017-09-12 22:30:35 +0430

    fix teacher card

--- resources/assets/js/staff/teacher/components/teacher-card.vue
+++ resources/assets/js/staff/teacher/components/teacher-card.vue
@@ -22,73 +22,36 @@
                          class="img-circle">
                 </div>
                 <div class="form-horizontal col-sm-9 col-xs-6">
-                    <div class="form-group col-md-6">
-                        <label class="col-md-4 col-sm-3 col-xs-4 control-label">ایمیل</label>
-                        <div class="col-md-8 col-sm-9 col-xs-8">
-                            <p class="form-control-static">{{ teacher.user.email }}</p>
+                    <div class="row">
+                        <div class="form-group col-sm-6">
+                            <label class="col-md-4 col-sm-3 col-xs-4 control-label">ایمیل</label>
+                            <div class="col-md-8 col-sm-9 col-xs-8">
+                                <p class="form-control-static">{{ teacher.user.email }}</p>
+                            </div>
                         </div>
-                    </div>
+                        <div class="form-group col-sm-6">
 
-                    <div class="form-group col-md-6">
-                        <label class="col-md-4 col-sm-3 col-xs-4 control-label">کد ملی</label>
-                        <div class="col-md-8 col-sm-9 col-xs-8">
-                            <p class="form-control-static">{{ teacher.user.national_code }}</p>
-                        </div>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label class="col-md-4 col-sm-3 col-xs-4 control-label">موبایل</label>
-                        <div class="col-md-8 col-sm-9 col-xs-8">
-                            <p class="form-control-static">{{ teacher.mobile }}</p>
-                        </div>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label class="col-md-4 col-sm-3 col-xs-4 control-label">مدرک</label>
-                        <div class="col-md-8 col-sm-9 col-xs-8">
-                            <!--<p class="form-control-static">{{ teacher.degree }}</p>-->
-                            <div v-if="teacher.degree == 'below diploma' " class="">
-                                <p class="form-control-static">زیر دیپلم</p>
-                            </div>
-                            <div v-if="teacher.degree == 0 " class="">
-                                <p class="form-control-static">زیر دیپلم</p>
-                            </div>
-                            <div v-if="teacher.degree == 'diploma' " class="">
-                                <p class="form-control-static">دیپلم</p>
-                            </div>
-                            <div v-if="teacher.degree == 1" class="">
-                                <p class="form-control-static">دیپلم</p>
-                            </div>
-                            <div v-if="teacher.degree == 'Ad'" class="">
-                                <p class="form-control-static">فوق دیپلم</p>
-                            </div>
-                            <div v-if="teacher.degree == 2 " class="">
-                                <p class="form-control-static">فوق دیپلم</p>
-                            </div>
-                            <div v-if="teacher.degree == 'BsC'" class="">
-                                <p class="form-control-static">کارشناسی</p>
-                            </div>
-                            <div v-if="teacher.degree == 3 " class="">
-                                <p class="form-control-static">کارشناسی</p>
-                            </div>
-                            <div v-if="teacher.degree == 'MsC'  " class="">
-                                <p class="form-control-static">کارشناسی ارشد</p>
-                            </div>
-                            <div v-if="teacher.degree == 4 " class="">
-                                <p class="form-control-static">کارشناسی ارشد</p>
-                            </div>
-                            <div v-if="teacher.degree == 5 " class="">
-                                <p class="form-control-static">دکتری</p>
-                            </div>
-                            <div v-if="teacher.degree == 'PhD'  " class="">
-                                <p class="form-control-static">دکتری</p>
-                            </div>
-                            <div v-if="teacher.degree == 'Other' " class="">
-                                <p class="form-control-static">موارد دیگر</p>
-                            </div>
-                            <div v-if="teacher.degree == 6 " class="">
-                                <p class="form-control-static">موارد دیگر</p>
+                            <label class="col-md-4 col-sm-3 col-xs-4 control-label">کد ملی</label>
+                            <div class="col-md-8 col-sm-9 col-xs-8">
+                                <p class="form-control-static">{{ teacher.user.national_code }}</p>
                             </div>
                         </div>
                     </div>
+                    <div class="row">
+                        <div class="form-group col-sm-6">
+                            <label class="col-md-4 col-sm-3 col-xs-4 control-label">موبایل</label>
+                            <div class="col-md-8 col-sm-9 col-xs-8">
+                                <p class="form-control-static">{{ teacher.mobile }}</p>
+                            </div>
+                        </div>
+                        <div class="form-group col-sm-6">
+                            <label class="col-xs-6 control-label">سطح تحصیلات</label>
+                            <div class="col-xs-6">
+                                <degree-level-paragraph v-model="teacher.degree"></degree-level-paragraph>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
                 </div>
             </div>
         </div>
